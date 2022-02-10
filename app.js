de49786b7c37942cf8e97c6b685ab1465dc54db9
Tayfun Altıparmak <91414101+taltiparmakk@users.noreply.github.com>
2022-02-10 12:49:58 +0300

    fotoğraf ekleme modulu eklendi

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const path = require("path");
 const ejs = require('ejs');
 const app = express();
+const fs = require("fs");
 const Photo = require("./models/Photo");
 
 
@@ -20,14 +22,16 @@ app.set("view engine", "ejs");
 //MIDDLEWARES
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); //gönderilen verileri encoding ediyor
-app.use(express.json())
+app.use(express.json());
+app.use(fileUpload());
+
 
 
 //ROUTES
 // fotoğrafların asenkron gitmeisin isitiyorum
 //Anasayfaya fotoları göndermek için
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
+  const photos = await Photo.find({}).sort("-dateCreated");
   res.render('index', {
     photos
   });
@@ -35,13 +39,41 @@ app.get('/', async (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about');
 });
+///fotolara gelen unique idleri yakalayıp tıklayınca açmak için
+app.get('/photos/:id', async (req, res) => {
+  // res.render('about');
+  const photo = await Photo.findById(req.params.id)
+  res.render("photo", {
+    photo
+  })
+});
+
 app.get('/add', (req, res) => {
   res.render('add');
 });
 //bodydeb gelen requestler
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body) 
-  res.redirect("/");
+  // await Photo.create(req.body)
+  // res.redirect("/");
+
+  const uploadDir = "public/uploads";
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+  }
+
+  let uploadedImage = req.files.image
+  let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name
+
+  uploadedImage.mv(uploadPath,
+    async () => {
+      await Photo.create({
+        ...req.body,
+        image: "/uploads/" + uploadedImage.name
+      });
+      res.redirect("/");
+    });
+
 });
 
 //app listener yazıyoruz
