2b554d7b95ab923441086b073e0dd86030615f3d
Tayfun Altıparmak <91414101+taltiparmakk@users.noreply.github.com>
2022-02-10 18:09:56 +0300

    mvcye göre düzenlendi

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 const methodOverride = require('method-override');
-const path = require("path");
 const ejs = require('ejs');
 const app = express();
-const fs = require("fs");
-const Photo = require("./models/Photo");
-
+const photoController = require("./controllers/photoController")
+const pageController = require("./controllers/pageController")
 
 //connect DB
 mongoose.connect("mongodb://localhost/pcat-test-db", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  
 });
 
 
@@ -25,75 +24,26 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); //gönderilen verileri encoding ediyor
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method', {
+  methods: ["POST", "GET"]
+}));
 
 
-
+//PHOTO CONTROLLERS
 //ROUTES
 // fotoğrafların asenkron gitmeisin isitiyorum
 //Anasayfaya fotoları göndermek için
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({}).sort("-dateCreated");
-  res.render('index', {
-    photos
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-///fotolara gelen unique idleri yakalayıp tıklayınca açmak için
-app.get('/photos/:id', async (req, res) => {
-  // res.render('about');
-  const photo = await Photo.findById(req.params.id)
-  res.render("photo", {
-    photo
-  })
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete("/photos/:id", photoController.deletePhoto);
 
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-//bodydeb gelen requestler
-app.post('/photos', async (req, res) => {
-  // await Photo.create(req.body)
-  // res.redirect("/");
+//PAGE CONTROLLERS
+app.get('/about', pageController.getAboutPage );
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
-  const uploadDir = "public/uploads";
-
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir)
-  }
-
-  let uploadedImage = req.files.image
-  let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name
-
-  uploadedImage.mv(uploadPath,
-    async () => {
-      await Photo.create({
-        ...req.body,
-        image: "/uploads/" + uploadedImage.name
-      });
-      res.redirect("/");
-    });
-
-});
-
-app.get('/photos/edit/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id })
-
-  res.render('edit', {
-    photo
-  });
-});
-
-app.put('/photos/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title
-  photo.description = req.body.description
-  photo.save()
-  res.redirect(`/photos/${req.params.id}`)
-
-});
 //app listener yazıyoruz
 const port = 3000;
 app.listen(port, () => {
