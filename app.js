31f82070833a7a593e5c5393ddfa64a482010331
Tayfun Altıparmak <91414101+taltiparmakk@users.noreply.github.com>
2022-02-10 14:12:16 +0300

    foto update eklendi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const path = require("path");
 const ejs = require('ejs');
 const app = express();
@@ -24,6 +25,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); //gönderilen verileri encoding ediyor
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 
 
@@ -76,6 +78,22 @@ app.post('/photos', async (req, res) => {
 
 });
 
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id })
+
+  res.render('edit', {
+    photo
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+  res.redirect(`/photos/${req.params.id}`)
+
+});
 //app listener yazıyoruz
 const port = 3000;
 app.listen(port, () => {
